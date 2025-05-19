38657fcb3bde82e954bcff86a5b85434598f2e4b
Pylaeva Svetlana <pl.svetlanaa.a@gmail.com>
2025-05-19 11:18:44 +0300

    add

---
+++ Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace CG_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.glControl1 = new OpenTK.GLControl();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.trackBar6 = new System.Windows.Forms.TrackBar();
+            this.trackBar5 = new System.Windows.Forms.TrackBar();
+            this.trackBar4 = new System.Windows.Forms.TrackBar();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // glControl1
+            // 
+            this.glControl1.BackColor = System.Drawing.Color.Black;
+            this.glControl1.Location = new System.Drawing.Point(15, 16);
+            this.glControl1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.glControl1.Name = "glControl1";
+            this.glControl1.Size = new System.Drawing.Size(593, 495);
+            this.glControl1.TabIndex = 0;
+            this.glControl1.VSync = false;
+            this.glControl1.Load += new System.EventHandler(this.glControl1_Load);
+            this.glControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.glControl1_Paint);
+            this.glControl1.Resize += new System.EventHandler(this.glControl1_Resize);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.trackBar6);
+            this.groupBox2.Controls.Add(this.trackBar5);
+            this.groupBox2.Controls.Add(this.trackBar4);
+            this.groupBox2.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.groupBox2.Location = new System.Drawing.Point(649, 52);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox2.Size = new System.Drawing.Size(497, 286);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Координаты камеры";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(396, 192);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 30);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Z";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(394, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 30);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Y";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(394, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 30);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "X";
+            // 
+            // trackBar6
+            // 
+            this.trackBar6.Location = new System.Drawing.Point(25, 192);
+            this.trackBar6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.trackBar6.Minimum = -10;
+            this.trackBar6.Name = "trackBar6";
+            this.trackBar6.Size = new System.Drawing.Size(363, 69);
+            this.trackBar6.TabIndex = 2;
+            this.trackBar6.Scroll += new System.EventHandler(this.trackBar6_Scroll);
+            // 
+            // trackBar5
+            // 
+            this.trackBar5.Location = new System.Drawing.Point(24, 115);
+            this.trackBar5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.trackBar5.Minimum = -10;
+            this.trackBar5.Name = "trackBar5";
+            this.trackBar5.Size = new System.Drawing.Size(363, 69);
+            this.trackBar5.TabIndex = 1;
+            this.trackBar5.Scroll += new System.EventHandler(this.trackBar5_Scroll);
+            // 
+            // trackBar4
+            // 
+            this.trackBar4.Location = new System.Drawing.Point(22, 38);
+            this.trackBar4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.trackBar4.Minimum = -10;
+            this.trackBar4.Name = "trackBar4";
+            this.trackBar4.Size = new System.Drawing.Size(364, 69);
+            this.trackBar4.TabIndex = 0;
+            this.trackBar4.Scroll += new System.EventHandler(this.trackBar4_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(0)))), ((int)(((byte)(40)))));
+            this.ClientSize = new System.Drawing.Size(1205, 546);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.glControl1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private OpenTK.GLControl glControl1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TrackBar trackBar6;
+        private System.Windows.Forms.TrackBar trackBar5;
+        private System.Windows.Forms.TrackBar trackBar4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace CG_3
+{
+    public partial class Form1 : Form
+    {
+        public OpenTK.Vector3 CameraPosition;
+
+        //public float Depth;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void glControl1_Paint(object sender, PaintEventArgs e)
+        {
+            Draw();
+        }
+
+        private void glControl1_Load(object sender, EventArgs e)
+        {
+            View.Init();
+            View.InitShaders();
+        }
+
+        private void glControl1_Resize(object sender, EventArgs e)
+        {
+            GL.Viewport(0, 0, glControl1.Width, glControl1.Height);
+            glControl1.Invalidate();
+        }
+
+        private void SetUniformVec3(string name, OpenTK.Vector3 value)
+        {
+            GL.Uniform3(GL.GetUniformLocation(View.BasicProgramID, name), value);
+        }
+
+        private void SetUniform1f(string name, float value)
+        {
+            GL.Uniform1(GL.GetUniformLocation(View.BasicProgramID, name), value);
+        }
+
+        private void Draw()
+        {
+            GL.ClearColor(Color.White);                 // устанавливает цвет, которым будет очищен цветовой буфер
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            GL.UseProgram(View.BasicProgramID);          // активирует шейдерную программу для использования
+
+            // передача параметров камеры в шейдеры
+            SetUniformVec3("camera_position", CameraPosition);
+
+            // отрисовка фона
+            GL.Color3(Color.White);
+            GL.Begin(PrimitiveType.Quads);
+
+            GL.TexCoord2(0, 1);
+            GL.Vertex2(-1, -1);
+
+            GL.TexCoord2(1, 1);
+            GL.Vertex2(1, -1);
+
+            GL.TexCoord2(1, 0);
+            GL.Vertex2(1, 1);
+
+            GL.TexCoord2(0, 0);
+            GL.Vertex2(-1, 1);
+
+            GL.End();
+
+            glControl1.SwapBuffers(); //копируем содержимое буфера вне экрана в буфер на экране
+            GL.UseProgram(0);
+        }
+
+        private void trackBar4_Scroll(object sender, EventArgs e)
+        {
+            CameraPosition.X = trackBar4.Value;
+            glControl1.Invalidate();
+        }
+
+        private void trackBar5_Scroll(object sender, EventArgs e)
+        {
+            CameraPosition.Y = trackBar5.Value;
+            glControl1.Invalidate();
+        }
+
+        private void trackBar6_Scroll(object sender, EventArgs e)
+        {
+            CameraPosition.Z = trackBar6.Value;
+            glControl1.Invalidate();
+        }
+
+    }
+}
